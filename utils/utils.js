c8075ec68a8dc39f8a47b3c0e7aff418725cd578
arthas <satyam.nitt@gmail.com>
2016-09-15 11:40:56 +0530

    basic auth in middleware

---
+++ utils/utils.js
@@ -0,0 +1,11 @@
+exports.Authenticate = function(auth_token){
+
+    console.log("inside authentication zone....")
+    if (auth_token == "hmmm"){
+        return true
+    }
+
+    else {return false}
+
+
+}
--- zion.js
+++ zion.js
@@ -1,19 +1,58 @@
-io = require('socket.io')(8001);
+io = require('socket.io')(8000);
+utils = require('./utils/utils.js');
+
+console.log("socket listening on 8000");
 
 
-console.log("socket listening on 8001");
-io.on('connection', function (socket) {
+
+
+
+io.use(function (socket, next){
+
+
+
+    console.log("init data -> "+ JSON.stringify(socket.handshake.query.auth_token));
+    auth_token = socket.handshake.query.auth_token
+    if(!utils.Authenticate(auth_token)){
+
+        console.log("Authentication failed...");
+
+        // next(new Error('Authentication error'));
+        next(new Error("Authentication error"));
+        socket.disconnect();
+    }
+    else{
+
+    next()
+
+    }
+
+    // utils.handlePreConnect(socket, next);
+
+
+
+});
+
+
+
+
+io.on('connection', function (socket, callback) {
+
 
 
     console.log("New connection - "+socket.id);
+    socket.disconnect();
+    console.log("Disconnected...")
 
 
-  socket.on('get_online_count', function (data) {
+
+
+    socket.on('get_online_count', function (data) {
 
     console.log("sending online count 99 to onlinecount");
     socket.emit('onlinecount', "99");
 
-  });
+    });
 
 
 });
